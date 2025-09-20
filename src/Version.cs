89005746588e82850b1dc54ba2f5fc71a77bdf85
derkalle4 <kalle@kandru.de>
2025-09-20 16:49:18 +0200

    bump version to 0.0.8

--- src/Version.cs
+++ src/Version.cs
@@ -2,6 +2,6 @@ namespace GrenadeInfo
 {
     public partial class GrenadeInfo
     {
-        public override string ModuleVersion => "0.0.7";
+        public override string ModuleVersion => "0.0.8";
     }
 }
